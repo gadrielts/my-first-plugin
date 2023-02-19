42c075e0fc67b48cfba82ef67bd3c72f85fb4b25
pedrx07 <pedrx07@outlook.com>
2023-02-19 15:27:22 -0300

    Add onQuit on PlayerEvent

--- src/main/java/me/gadriel/listeners/PlayerEvent.java
+++ src/main/java/me/gadriel/listeners/PlayerEvent.java
@@ -4,6 +4,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class PlayerEvent implements Listener {
 
@@ -12,5 +13,12 @@ public class PlayerEvent implements Listener {
         Player player = event.getPlayer();
 
         event.setJoinMessage("Wow!! " + player.getName() + " joined the server.");
+    }
+
+    @EventHandler
+    public void onLeavePlayer(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+
+        event.setQuitMessage("Sad... " + player.getName() + " leave the server.");
     }
 }
